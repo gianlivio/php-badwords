fee6b7c6f1a95dfb6d31bd188aba29bb145c85c4
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-05-21 15:10:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <form action="server.php">
+        <textarea name="paragraph" id=""></textarea>
+        <label for="word">Inserisci parola da nascondere</label>
+        <input type="text" name="word">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,36 @@
+<?php 
+$text = $_GET["paragraph"];
+$bad_word = $_GET["word"];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<span> Il tuo paragrafo è il seguente: </span>
+<?php 
+echo "$text";
+?>
+
+<span> Il paragrafo è lungo: </span>
+<?php 
+echo strlen ("$text");
+?>
+
+<span> Il paragrafo con la parola nascosta è il seguente: </span>
+<?php 
+$text = str_ireplace("$bad_word", "***", "$text");
+echo "$text";
+?>
+
+
+    
+</body>
+</html>
